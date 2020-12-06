9d14319b173262364738c0348c1ad6acc02732a9
Hien Tran <tnhien@apcs.vn>
2020-12-06 15:45:30 +0700

    setup Loopback

---
+++ backend/boot/major.js
@@ -0,0 +1,12 @@
+const major = [
+  "Toán - Tin học",
+  "Vật lý - Địa cầu và Hải dương",
+  "Hóa học",
+  "Sinh học - Công nghệ sinh học",
+  "Địa chất và Tài nguyên Trái đất",
+  "Môi trường",
+  "Công nghệ Thông tin - Truyền thông",
+  "Điện tử - Viễn thông",
+  "Khoa học và Công nghệ Vật liệu",
+  "Kỹ thuật hạt nhân và Vật lý Y khoa",
+];
