317b73f939bb48742583e3b0d8557fcef03c4e8d
Quỳnh Châu <lvqchau@apcs.vn>
2020-12-10 05:59:12 +0700

    init schedule recap

---
+++ src/pages/Home/ScheduleRecap/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ScheduleRecap = () => {
+  return (
+    <div style={{margin: 25, height: 100, background: 'white'}}>
+      
+    </div>
+  )
+}
+
+export default ScheduleRecap
