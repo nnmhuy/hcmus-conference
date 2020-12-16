ffdca4f841b17c84b1d5526adaf49bba751c4d38
Nguyễn Ngọc Minh Huy <nnmhuy@apcs.vn>
2020-12-16 14:16:09 +0700

    pm2 config

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    script: 'cd backend && npm run heroku-cleanup && npm run start',
+    watch: '.',
+    instances: 'max',
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host: '159.89.200.152',
+      ref  : 'origin/release',
+      repo: 'git@github.com:nnmhuy/hcmus-conference.git',
+      path : './hcmus-conference',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      "env": {
+        "NODE_ENV": "production"
+      }
+    }
+  }
+};
