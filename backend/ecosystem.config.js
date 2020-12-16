40eb7bf8fc7edcd729d28f51febd6e5542492284
Nguyễn Ngọc Minh Huy <nnmhuy@apcs.vn>
2020-12-16 14:37:27 +0700

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       host: '159.89.200.152',
       ref  : 'origin/release',
       repo: 'git@github.com:nnmhuy/hcmus-conference.git',
-      path : './',
+      path : './hcmus-conference',
       'pre-setup': '',
       'pre-deploy-local': '',
       // 'post-deploy' : 'cd backend && npm install && pm2 reload ecosystem.config.js --env production',
