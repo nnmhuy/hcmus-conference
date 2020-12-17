702f7f8197adeac3a5410f109b2e3cd110eba80b
Huu Duc <yellowthundercat@gmail.com>
2020-12-17 20:07:15 +0700

    aa

---
+++ src/pages/Schedule/components/TimeSessionList.js
@@ -0,0 +1,28 @@
+let timeSessionList = [
+  {
+    startDate: new Date("December 18, 2020 6:30:00"),
+    endDate: new Date("December 18, 2020 12:55:00"),
+    day: 1,
+    name: 'Buổi sáng'
+  },
+  {
+    startDate: new Date("December 18, 2020 13:00:00"),
+    endDate: new Date("December 18, 2020 19:30:00"),
+    day: 2,
+    name: 'Buổi chiều'
+  },
+  {
+    startDate: new Date("December 19, 2020 6:30:00"),
+    endDate: new Date("December 19, 2020 12:55:00"),
+    day: 3,
+    name: 'Buổi sáng'
+  },
+  {
+    startDate: new Date("December 19, 2020 13:00:00"),
+    endDate: new Date("December 19, 2020 19:30:00"),
+    day: 4,
+    name: 'Buổi chiều'
+  }
+]
+
+export default timeSessionList
