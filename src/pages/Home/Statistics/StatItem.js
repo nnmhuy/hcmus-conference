79ab56b5fa55d3f2a00a779a588eac0007f4aaba
Hien Tran <tnhien@apcs.vn>
2020-12-18 03:08:46 +0700

    update Statistic component

--- src/pages/Home/Statistics/StatItem.js
+++ src/pages/Home/Statistics/StatItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Ticker from './Ticker'
+// import Ticker from './Ticker'
 
 const CenteredText = styled.div`
   text-align: center;
@@ -22,12 +22,22 @@ const Title = styled.p`
   font-weight: 400;
   text-transform: uppercase;
 `
+const Ticker = styled.h3`
+  font-size: 4rem;
+  color: white;
+  font-weight: 700;
+  margin-bottom: 0;
+  @media (max-width: 576px) {
+    font-size: 3.2rem;
+  }
+`
 
 function StatItem(props) {
   const { count, label } = props
   return (
     <CenteredText>
-      <Ticker end={count} />
+       {/* <Counter ref={countUpRef}/> */}
+      <Ticker>{count}</Ticker>
       <Title>{label}</Title>
     </CenteredText>
   )
